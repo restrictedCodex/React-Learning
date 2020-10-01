b7e6c1588172eccc26fe3467971bad82c030495b
charudutta <restrictedcharudutta@gmail.com>
2020-10-01 21:25:04 +0530

    added parcel, prettier, json files and server setup

---
+++ Adopt Me/src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
